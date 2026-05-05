c61645117897f058f3faddf2fc7c1431c1742ecd
I-D-D-I <3711080@gmail.com>
2026-05-05 19:02:49 +0300

    Поправил в LoginPage локатор LOGIN_BUTTON_SIGN_IN

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -10,8 +10,7 @@ class LoginPageLocators:
     LOGIN_BUTTON = (By.XPATH, '//*[@data-test-id="enter-action"]')
     LOGIN_BUTTON_BY_QR = (By.XPATH, '//*[@label="Войти по QR-коду"]')
     LOGIN_BUTTON_NOT_ENTER = (By.XPATH, '//*[@aria-label="Не получается войти?"]')
-    LOGIN_BUTTON_SIGN_IN = (
-        By.XPATH, '//*[@class="vkuiInternalTappable vkuiButton__host vkuiButton__sizeL vkuiButton__modeSecondary vkuiButton__appearanceNeutral vkuiButton__sizeYNone vkuiButton__stretched vkuiTappable__host vkuiTappable__sizeXNone vkuiTappable__hasPointerNone vkuiClickable__host vkuiClickable__realClickable vkuistyles__-focus-visible vkuiRootComponent__host"]')
+    LOGIN_BUTTON_SIGN_IN = (By.XPATH, '//span[text()="Зарегистрироваться"]')
     LOGIN_BUTTON_BY_VK = (By.XPATH, '//*[@class="i ic social-icon __s __vk_id"]')
     LOGIN_BUTTON_BY_MAIL = (By.XPATH, '//*[@class="i ic social-icon __s __mailru"]')
     LOGIN_BUTTON_BY_YANDEX = (By.XPATH, '//*[@class="i ic social-icon __s __yandex"]')
