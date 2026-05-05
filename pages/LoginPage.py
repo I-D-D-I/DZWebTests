3a27dda7626eb15127a85ea277a45efb0ac6d4f2
I-D-D-I <3711080@gmail.com>
2026-05-05 16:44:59 +0300

    Поправил в LoginPage локаторы

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,18 +1,21 @@
 from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
+
 class LoginPageLocators:
-    LOGIN_SELECT_ENTER = (By.XPATH, '//*[@id="login-687708430"]')
-    LOGIN_SELECT_ENTER_BY_QR = (By.XPATH, '//*[@id="qrCode-687708493"]')
+    LOGIN_SELECT_ENTER = (By.XPATH, '//*[@title="Вход"]')
+    LOGIN_SELECT_ENTER_BY_QR = (By.XPATH, '//*[@title="QR-код"]')
     LOGIN_FIELD = (By.ID, 'field_email')
     PASSWORD_FIELD = (By.ID, 'field_password')
     LOGIN_BUTTON = (By.XPATH, '//*[@data-test-id="enter-action"]')
-    LOGIN_BUTTON_BY_QR = (By.XPATH, '//*[@id="tabpanel-login-687708430"]/vkid-form-adapter/div/div/div/div[1]/button')
-    LOGIN_BUTTON_NOT_ENTER = (By.XPATH, '//*[@id="tabpanel-login-687708430"]/vkid-form-adapter/div/div/div/div[1]/span/button')
-    LOGIN_BUTTON_SIGN_IN = (By.XPATH, '//*[@id="tabpanel-login-687708430"]/vkid-form-adapter/div/div/div/div[3]/button')
-    LOGIN_BUTTON_BY_VK = (By.XPATH, '//*[@id="tabpanel-login-687708430"]/vkid-form-adapter/div/div/div/div[3]/div[4]/ui-part/div/a[1]/i')
-    LOGIN_BUTTON_BY_MAIL = (By.XPATH, '//*[@id="tabpanel-login-687708430"]/vkid-form-adapter/div/div/div/div[3]/div[4]/ui-part/div/a[2]/i')
-    LOGIN_BUTTON_BY_YANDEX = (By.XPATH, '//*[@id="tabpanel-login-687708430"]/vkid-form-adapter/div/div/div/div[3]/div[4]/ui-part/div/a[3]/i')
+    LOGIN_BUTTON_BY_QR = (By.XPATH, '//*[@label="Войти по QR-коду"]')
+    LOGIN_BUTTON_NOT_ENTER = (By.XPATH, '//*[@aria-label="Не получается войти?"]')
+    LOGIN_BUTTON_SIGN_IN = (
+        By.XPATH, '//*[@class="vkuiInternalTappable vkuiButton__host vkuiButton__sizeL vkuiButton__modeSecondary vkuiButton__appearanceNeutral vkuiButton__sizeYNone vkuiButton__stretched vkuiTappable__host vkuiTappable__sizeXNone vkuiTappable__hasPointerNone vkuiClickable__host vkuiClickable__realClickable vkuistyles__-focus-visible vkuiRootComponent__host"]')
+    LOGIN_BUTTON_BY_VK = (By.XPATH, '//*[@class="i ic social-icon __s __vk_id"]')
+    LOGIN_BUTTON_BY_MAIL = (By.XPATH, '//*[@class="i ic social-icon __s __mailru"]')
+    LOGIN_BUTTON_BY_YANDEX = (By.XPATH, '//*[@class="i ic social-icon __s __yandex"]')
+
 
 class LoginPageHelper(BasePage):
     pass
